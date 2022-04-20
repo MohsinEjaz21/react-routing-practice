4188720415184f1fd0778b5d66cf8957849060b1
Mohsin <44529081+MohsinEjaz21@users.noreply.github.com>
2022-04-20 17:01:54 +0500

    understanding relative path completed

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -8,6 +8,8 @@ function Products() {
       <nav>
         <Link to="featured" >Feature Product</Link>
         <Link to="new" >New Product</Link>
+        {/* Route to new can be done using absolute path like below */}
+        {/* <Link to="/products/new" >New Product</Link> */}
       </nav>
       <Outlet />
     </>
