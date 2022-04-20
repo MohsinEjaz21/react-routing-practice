ef6e2afb1e62e97b19a590ec516cb1ab06e3e2cc
Mohsin <44529081+MohsinEjaz21@users.noreply.github.com>
2022-04-20 16:44:33 +0500

    search params are used for filters in get request ... is completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,7 @@ function Navbar() {
       <NavLink to="/">Home</NavLink>
       <NavLink to="/about">About</NavLink>
       <NavLink to="/products">Products</NavLink>
+      <NavLink to="/users">Users</NavLink>
     </nav>
   )
 }
--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useSearchParams } from 'react-router-dom'
 
 function Users() {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const isActiveUsers = searchParams.get('filter') === 'active'
   return (
     <>
       <h1>Users</h1>
@@ -10,6 +12,11 @@ function Users() {
         <li><Link to="2">User 2</Link></li>
         <li><Link to="3">User 3</Link></li>
       </ul>
+      <div>
+        <button onClick={() => setSearchParams({ filter: 'active' })} >Active Users</button>
+        <button onClick={() => setSearchParams({})} >Reset Filters</button>
+      </div>
+      <h2>{isActiveUsers ? 'Show Active Users' : 'showing All users '}</h2>
       <Outlet />
     </>
   )
